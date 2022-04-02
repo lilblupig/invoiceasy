4c54d97fc5ab9c3e6544e2ea6925d5b8d49e6ce6
lilblupig <lilblupig@gmail.com>
2022-04-02 19:29:37 +0000

    Import dj database url in settings

--- invoiceasy/settings.py
+++ invoiceasy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -95,8 +96,9 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'invoiceasy.wsgi.application'
 
 
-# Database
+# DATABASE
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 if 'DATABASE_URL' in os.environ:
 
     DATABASES = {
@@ -106,10 +108,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
