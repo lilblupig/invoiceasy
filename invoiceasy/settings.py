3c219b673fbf409e15ab7abf41fa55ac13a0ea1d
lilblupig <lilblupig@gmail.com>
2022-04-10 20:29:55 +0000

    Fix PEP8 linter issue in settings

--- invoiceasy/settings.py
+++ invoiceasy/settings.py
@@ -170,7 +170,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = ('cloudinary_storage.storage.StaticHashed'
+                       'CloudinaryStorage')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
