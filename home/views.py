f1a0b13ed3e10b5dd6c28ee4f50b44820cd5e516
lilblupig <lilblupig@gmail.com>
2022-04-10 19:35:06 +0000

    Add About test page

--- home/views.py
+++ home/views.py
@@ -8,3 +8,8 @@ from django.shortcuts import render
 def index(request):
     """ View to return index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ View to return about page """
+    return render(request, 'home/about.html')
