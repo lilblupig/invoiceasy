8a654a4130f083d1a31e9b6d1e5cd1e84d633c3a
lilblupig <lilblupig@gmail.com>
2022-04-11 21:28:42 +0000

    Setup auto response and formatted admin mail

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.conf import settings
 from .forms import ContactForm
@@ -15,22 +16,45 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = 'InvoicEasy contact form submission.',
-            body = {
-                'first_name': form.cleaned_data['first_name'],
-                'last_name': form.cleaned_data['last_name'],
-                'email': form.cleaned_data['email'],
-                'telephone': form.cleaned_data['telephone'],
-                'message': form.cleaned_data['message'],
-            }
-            message = "\n".join(body.values())
+            web_team = [settings.EMAIL_HOST_USER]
+            subject = 'InvoicEasy contact form submission.'
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = [form.cleaned_data['email']]
+            telephone = form.cleaned_data['telephone']
+            message = form.cleaned_data['message']
+
+            enquiry = render_to_string(
+                'contact/emails/contact.txt',
+                {
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'email': email,
+                    'telephone': telephone,
+                    'message': message,
+                }
+            )
+
+            response = render_to_string(
+                'contact/emails/auto.txt',
+                {
+                    'first_name': first_name,
+                    'message': message,
+                }
+            )
 
             try:
                 send_mail(
                     subject,
-                    message,
-                    settings.DEFAULT_FROM_EMAIL,
-                    [settings.DEFAULT_FROM_EMAIL]
+                    enquiry,
+                    email,
+                    web_team
+                )
+                send_mail(
+                    subject,
+                    response,
+                    web_team,
+                    email
                 )
                 messages.success(request, 'Your message has been sent to the '
                                           'team, we will be in touch shortly.')
