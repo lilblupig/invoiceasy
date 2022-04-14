e86c4b5d6f30116ffa1c5bc86404a5b0f6018a2b
lilblupig <lilblupig@gmail.com>
2022-04-14 21:23:23 +0000

    Set up plans and admin info

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
+""" Admin information for product and pricing pages """
+
 from django.contrib import admin
+from .models import Plan
 
 # Register your models here.
+
+
+class PlanAdmin(admin.ModelAdmin):
+    """ Set fields to display in admin """
+    list_display = (
+        'name',
+        'price',
+    )
+
+
+admin.site.register(Plan, PlanAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
+""" Model information for product and pricing pages """
+
 from django.db import models
 
 # Create your models here.
+
+
+class Plan(models.Model):
+    """ Define data for plans """
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def get_name(self):
+        """ Return name of plan """
+        return self.name
