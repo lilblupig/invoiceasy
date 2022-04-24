08085d1b792b35ecbf6bc42411961f0b79c7290b
lilblupig <lilblupig@gmail.com>
2022-04-24 13:39:34 +0000

    Require login for subscription page

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,10 +1,12 @@
 """ View information for subscription pages """
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 
 
+@login_required
 def subscribe(request):
     """ View to return checkout page """
     return render(request, 'subscriptions/subscribe.html')
