e31845a19e44107fcdb124fe9d59294fdde4be98
lilblupig <lilblupig@gmail.com>
2022-04-24 14:24:17 +0000

    Setup checkout session creation

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.subscribe, name='subscribe'),
     path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,6 +1,7 @@
 """ View information for subscription pages """
+# From https://testdriven.io/blog/django-stripe-subscriptions/
 
-
+import stripe
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse
@@ -22,3 +23,28 @@ def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publicKey': settings.STRIPE_PUBLIC_KEY}
         return JsonResponse(stripe_config, safe=False)
+
+
+@csrf_exempt
+def create_checkout_session(request):
+    """ Create a checkout session """
+    if request.method == 'GET':
+        domain_url = 'http://localhost:8000/'
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                client_reference_id=request.user.id if request.user.is_authenticated else None,
+                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'cancel/',
+                payment_method_types=['card'],
+                mode='subscription',
+                line_items=[
+                    {
+                        'price': settings.STRIPE_PRICE_ID,
+                        'quantity': 1,
+                    }
+                ]
+            )
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
