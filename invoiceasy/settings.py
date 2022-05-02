383f1b19d11fcacd5e5821e3d7848478f5ed0536
lilblupig <lilblupig@gmail.com>
2022-05-02 09:06:24 +0000

    Remove obsolete stripe test price

--- invoiceasy/settings.py
+++ invoiceasy/settings.py
@@ -208,5 +208,3 @@ if 'DEVELOPMENT' in os.environ:
     DOMAIN_URL = 'https://8000-lilblupig-invoiceasy-1i26yyobyrv.ws-eu43.gitpod.io/'
 else:
     DOMAIN_URL = 'https://invoiceasy.herokuapp.com/'
-
-STRIPE_PRICE_ID = 'price_1KpalFG7L1c3WfQs6dpYnVnM'
