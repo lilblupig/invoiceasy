1bc2591587d3d3c45ab7df39e27b050580b45b3a
lilblupig <lilblupig@gmail.com>
2022-05-02 09:49:27 +0000

    Add caching and compression

--- invoiceasy/settings.py
+++ invoiceasy/settings.py
@@ -174,6 +174,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Tell collectstatic where to find extra files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
