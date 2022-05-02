2c9237379a321a50f0263b76f1f2704d98f11ea9
lilblupig <lilblupig@gmail.com>
2022-05-02 07:25:32 +0000

    Add logging to settings.py

--- invoiceasy/settings.py
+++ invoiceasy/settings.py
@@ -27,6 +27,25 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'handlers': {
+      'file': {
+         'level': 'DEBUG',
+         'class': 'logging.FileHandler',
+         'filename': '/tmp/debug.log',
+      },
+   },
+   'loggers': {
+      'django': {
+         'handlers': ['file'],
+         'level': 'DEBUG',
+         'propagate': True,
+      },
+   },
+}
+
 ALLOWED_HOSTS = ['invoiceasy.herokuapp.com', 'localhost']
 
 
