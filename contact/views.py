c7c0a5b255081650e5d4ec104dbdbb2b3a7ec438
lilblupig <lilblupig@gmail.com>
2022-05-17 20:38:38 +0000

    Tidy comments and linter errors for Contact App

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,6 @@
 """ View information for contact page """
 
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
 from django.template.loader import render_to_string
@@ -14,6 +14,7 @@ from .forms import ContactForm
 
 def contact(request):
     """ View to return contact page """
+    # If request is POST get form fields and set as variables
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -25,6 +26,7 @@ def contact(request):
             telephone = form.cleaned_data['telephone']
             message = form.cleaned_data['message']
 
+            # Prepare data for enquiry sent to admin
             enquiry = render_to_string(
                 'contact/emails/contact.txt',
                 {
@@ -36,6 +38,7 @@ def contact(request):
                 }
             )
 
+            # Prepare data for auto response sent to user
             response = render_to_string(
                 'contact/emails/auto.txt',
                 {
@@ -44,6 +47,7 @@ def contact(request):
                 }
             )
 
+            # Send both emails and redirect to contact form
             try:
                 send_mail(
                     subject,
@@ -65,7 +69,9 @@ def contact(request):
                 return HttpResponse('Invalid header found.')
             return redirect('contact')
 
+    # If request is GET, check if user signed in
     if request.user.is_authenticated:
+        # If signed in, get user info and populate form
         user_profile = UserProfile.objects.get(user=request.user)
         form = ContactForm(initial={
             'first_name': user_profile.first_name,
@@ -74,6 +80,7 @@ def contact(request):
             'telephone': user_profile.telephone,
             })
     else:
+        # If not signed in, render blank form
         form = ContactForm()
 
     return render(request, 'contact/contact.html', {'form': form})
