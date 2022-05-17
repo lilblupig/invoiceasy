b7522e4692d1de20676baa12dce701e5c2fe7d44
lilblupig <lilblupig@gmail.com>
2022-05-17 21:29:24 +0000

    Tidy comments and linter errors for Invoices App

--- invoices/models.py
+++ invoices/models.py
@@ -8,11 +8,18 @@ from django.conf import settings
 
 class InvoiceCustomer(models.Model):
     """ Store user customer info """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     customer_code = models.CharField(max_length=10)
     customer_first_name = models.CharField(max_length=50)
     customer_last_name = models.CharField(max_length=50)
-    customer_business_name = models.CharField(max_length=50, null=True, blank=True)
+    customer_business_name = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+    )
     customer_address_1 = models.CharField(max_length=50)
     customer_address_2 = models.CharField(max_length=50, null=True, blank=True)
     customer_town_or_city = models.CharField(max_length=50)
@@ -27,8 +34,14 @@ class InvoiceCustomer(models.Model):
 
 class Invoice(models.Model):
     """ Store customer invoice info """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    customer_code = models.ForeignKey(to=InvoiceCustomer, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    customer_code = models.ForeignKey(
+        to=InvoiceCustomer,
+        on_delete=models.CASCADE
+    )
     invoice_number = models.CharField(max_length=20)
     invoice_date = models.CharField(max_length=20)
     invoice_info = models.TextField()
